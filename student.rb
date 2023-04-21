6e2e1d750f783886286d432427ead882af2d1cef
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-04-21 08:21:08 +0000

    Add class person and sub classes

--- student.rb
+++ student.rb
@@ -1,13 +1,10 @@
 class Student < Person
+  def initialize(classroom)
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
 
-    def initialize(classroom)
-
-        @classroom = classroom
-    end
-
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  def play_hooky
+    '¯(ツ)/¯'
+  end
 end
-
-
--- teacher.rb
+++ teacher.rb
@@ -1,11 +1,10 @@
 class Teacher < Person
+  def initialize(specialization)
+    super(name, age, parent_permission)
+    @specialization = specialization
+  end
 
-    def initialize(specialization)
-        @specialization  = specialization 
-
-    end
-
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
