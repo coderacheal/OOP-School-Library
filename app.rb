ea1e0667971513d392b163c39b0b5bf21f5ec8c7
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-04-22 15:53:16 +0000

    Implement app exit

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
+require_relative './library'
+
 class App
+  def initialize
+    @books = []
+    @rentals = []
+    @people = []
+  end
+
   def app_title
     puts 'Welcome to School Library App!'
   end
-  
+
   def all_options
     puts ''
     puts 'Please choose an option by entering a number'
@@ -13,5 +21,19 @@ class App
     puts '5 - Create a rental'
     puts '6 - List all rentals for a given persons id'
     puts '7 - Exit'
+  end
+
+  def list_all_books
+    puts 'No Books Available' if books.empty?
+    @books.each { |book| puts 'Title: #{book.title}, Author: #{book.author}'}
+  end
+
+  # def exceute_selected_option
+  #   if 
+  # end
+
+  def exit_library
+    puts 'Sad to see you go'
+    exit
   end
 end
--- main.rb
+++ main.rb
@@ -4,6 +4,15 @@ def main
   app = App.new
   app.app_title
   app.all_options
+
+  choice = gets.chomp
+  if choice == '7'
+    app.exit_library
+  else
+    exceute_selected_option
+  end
 end
+
+
 
 main
