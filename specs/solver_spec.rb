e56742f0e4f0fb6e055bf72d0335f5dc9f6618ef
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-04-28 19:49:30 +0000

    Factorial function working

--- specs/solver_spec.rb
+++ specs/solver_spec.rb
@@ -1,17 +1,22 @@
-require 'rspec'
-require './modules/sover'
+# require 'rspec'
+require_relative 'solver'
 
-describe "checking factorial solutions" do
-    
-    it 'return 1 when 0 is given' do 
-        expect(factorial).to eq(1) 
-    end 
+RSpec.describe Solver do
+    describe "checking factorial solutions" do
+        
+        it 'return 1 when 0 is given' do 
+            solver = Solver.new
+            expect(solver.factorial(0)).to eq(1) 
+        end 
 
-    it 'should return 1 when 1 is passed' do
-        expect(factorial).to  eq(1)
-    end
+        it 'should return 1 when 1 is passed' do
+            solver = Solver.new
+            expect(solver.factorial(1)).to  eq(1)
+        end
 
-    it "returns the correct value when given a positive integer" do
-        expect(factorial(5)).to eq(120)
+        it "returns the correct value when given a positive integer" do
+            solver = Solver.new
+            expect(solver.factorial(5)).to eq(120)
+        end
     end
 end
